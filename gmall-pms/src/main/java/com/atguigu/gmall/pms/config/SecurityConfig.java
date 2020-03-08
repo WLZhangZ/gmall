1d40c630eae34d78d1447a756605d2bdfc1f1987
WLZhangZ <18580186568@163.com>
2020-03-08 21:25:25 +0800

    pms 微服务运行成功

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.atguigu.gmall.pms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //所有访问都可以通过
+        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.csrf().disable();
+    }
+}
